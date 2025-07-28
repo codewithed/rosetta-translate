0935f24b63639ba7e3508085a8af1ad6e84c3dbb
Edmund Debrah <edmundking05@gmail.com>
2025-07-28 21:45:57 +0000

    update java docker images

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,2 +1,5 @@
-export const API_BASE_URL = 'http://192.168.8.112:8080/api';
-export const AUTH_BASE_URL = 'http://192.168.8.112:8080/auth';
+export const API_BASE_URL = "https://rosetta-translate.onrender.com:8080/api";
+export const AUTH_BASE_URL = "https://rosetta-translate.onrender.com:8080/auth";
+
+// export const API_BASE_URL = "http://192.168.8.112:8080/api";
+// export const AUTH_BASE_URL = "http://192.168.8.112:8080/auth";
